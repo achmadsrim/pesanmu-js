cd56901bcb4a6e5101295f68f8bb9a4fc2ff83ea
xiyyu <achmad.srim@gmail.com>
2020-11-03 02:25:51 +0700

    commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="hero">
+    <!-- Desktop -->
+    <div class="d-none d-md-block">
+      <div class="row mt-4">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Lezat Nya MenuKU,</strong>
+                <br />in Your Gadget 
+                <br />And Low Budget
+              </h2>
+              <p>Ayo segera pilih dan pesan makanan favorit Anda</p>
+              <router-link class="btn btn-lg btn-success" to="/foods">
+                <b-icon-arrow-right></b-icon-arrow-right> Pesan YUK
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <img src="../assets/images/hero.png" width="100%" />
+        </div>
+      </div>
+    </div>
+
+    <!-- Mobile -->
+    <div class="d-sm-block d-md-none">
+      <div class="row mt-4">
+        <div class="col-md-6 mb-3">
+          <img src="../assets/images/hero.png" width="100%" />
+        </div>
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Lezat Nya MenuKU,</strong>
+                <br />in Your Gadget 
+                <br />And Low Budget
+              </h2>
+              <p>Ayo segera pilih dan pesan makanan favorit Anda</p>
+              <button class="btn btn-lg btn-success">
+                <b-icon-arrow-right></b-icon-arrow-right> Pesan YUK
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style>
+</style>
